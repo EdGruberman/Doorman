8e4246731a8cb151904dd18c69c80f9bb7ec9c1d
EdGruberman <ed@rjump.com>
2012-11-30 15:09:53 -0700

    FIX: Remove comma from page parameter display

--- src/edgruberman/bukkit/doorman/Main.java
+++ src/edgruberman/bukkit/doorman/Main.java
@@ -24,7 +24,7 @@ public final class Main extends CustomPlugin {
 
     @Override
     public void onLoad() {
-        this.putConfigMinimum(CustomPlugin.CONFIGURATION_FILE, "1.2.0");
+        this.putConfigMinimum(CustomPlugin.CONFIGURATION_FILE, "1.2.1");
         this.setPathSeparator('|');
     }
 
